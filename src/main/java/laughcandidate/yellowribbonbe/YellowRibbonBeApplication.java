606452d61c9878c50be9419e5ee3b1aa1d7f3417
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-25 16:27:50 +0900

    feat: initialize project settings

---
+++ src/main/java/laughcandidate/yellowribbonbe/YellowRibbonBeApplication.java
@@ -0,0 +1,13 @@
+package laughcandidate.yellowribbonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowRibbonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowRibbonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/laughcandidate/yellowribbonbe/YellowRibbonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package laughcandidate.yellowribbonbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowRibbonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
