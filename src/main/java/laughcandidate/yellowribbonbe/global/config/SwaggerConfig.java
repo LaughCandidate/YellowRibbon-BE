6a05bbb9a6e137569f420189a4edc663ff3c05a8
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-31 00:35:39 +0900

    feat: #1 set swagger config

---
+++ src/main/java/laughcandidate/yellowribbonbe/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package laughcandidate.yellowribbonbe.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+@Profile({"local", "dev", "prod"})
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("Yellow Ribbon Project API Document")
+				.description("Yellow Ribbon 서버 API 명세서입니다.")
+				.version("v1.0.0")
+			).components(authComponent());
+	}
+
+	private Components authComponent() {
+		return new Components().addSecuritySchemes("jwt",
+			new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.in(SecurityScheme.In.HEADER)
+				.name("Authorization")
+				.scheme("Bearer")
+				.bearerFormat("JWT"));
+	}
+}
+
