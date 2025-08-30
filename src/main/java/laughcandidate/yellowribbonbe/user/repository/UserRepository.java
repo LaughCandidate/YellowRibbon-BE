4c3eb36bd798cf7feda384f839373d54aee9c070
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-31 00:32:50 +0900

    feat: #1 set user domain

---
+++ src/main/java/laughcandidate/yellowribbonbe/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package laughcandidate.yellowribbonbe.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import laughcandidate.yellowribbonbe.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByLoginIdAndIsDeletedFalse(String loginId);
+}
