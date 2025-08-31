4487de2830b9395389b3d556b98aba76144ad38e
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-31 14:08:34 +0900

    feat: #1 send sms verification code

---
+++ src/main/java/laughcandidate/yellowribbonbe/auth/constants/EmailVerificationConstant.java
@@ -0,0 +1,16 @@
+package laughcandidate.yellowribbonbe.auth.constants;
+
+public class EmailVerificationConstant {
+
+	public static final int VERIFICATION_TIME = 10;
+	public static final String VERIFICATION_CODE_PATTERN = "\\b(\\d{6})\\b";
+	public static final String PREFIX_VERIFICATION_CODE = "code_";
+	public static final String PREFIX_VERIFICATION_TIME = "time_";
+	public static final String IMAP_PROTOCOL = "imaps";
+	public static final String INBOX = "INBOX";
+	public static final String AT = "@";
+	public static final String TEXT = "text/plain";
+	public static final String MULTIPART = "multipart/*";
+	public static final String TXT = ".txt";
+	public static final String VERIFIED = "verified";
+}
