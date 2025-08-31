242489715bf3250fdf8cc08a42191de55b2c0e87
bronze_yun_4521 <ehddbs452100@naver.com>
2025-08-31 22:49:05 +0900

    feat: #4 set business domain

---
+++ src/main/java/laughcandidate/yellowribbonbe/business/entity/Business.java
@@ -0,0 +1,55 @@
+package laughcandidate.yellowribbonbe.business.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import laughcandidate.yellowribbonbe.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "BUSINESS")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Business {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "business_id")
+	private Long id;
+
+	@Column(name = "business_no", unique = true)
+	private String businessNo;
+
+	@Column(name = "owner_name")
+	private String ownerName;
+
+	@Column(name = "start_date")
+	private LocalDate startDate;
+
+	@Column(name = "business_name")
+	private String businessName;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Builder
+	public Business(String businessName, String businessNo, String ownerName, LocalDate startDate, User user) {
+		this.businessName = businessName;
+		this.businessNo = businessNo;
+		this.ownerName = ownerName;
+		this.startDate = startDate;
+		this.user = user;
+	}
+}
