b307fad070b874a06e1f907f3040436ed64d0743
bronze_yun_4521 <ehddbs452100@naver.com>
2025-09-03 21:01:35 +0900

    refactor: #10 change position

--- src/main/java/laughcandidate/yellowribbonbe/ai/api/BusinessValidationClient.java
+++ src/main/java/laughcandidate/yellowribbonbe/ai/api/BusinessValidationClient.java
@@ -1,4 +1,4 @@
-package laughcandidate.yellowribbonbe.business.client;
+package laughcandidate.yellowribbonbe.ai.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/laughcandidate/yellowribbonbe/business/config/FeignConfig.java
+++ src/main/java/laughcandidate/yellowribbonbe/business/config/FeignConfig.java
@@ -4,6 +4,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(basePackages = "laughcandidate.yellowribbonbe.business.client")
+@EnableFeignClients(basePackages = {"laughcandidate.yellowribbonbe.ai.api", "laughcandidate.yellowribbonbe.business"})
 public class FeignConfig {
 }
