636f6fdf6c1199aec0c52e1d0456f89b1d729b47
hwajin <hvvxzin99@gmail.com>
2025-09-07 03:00:04 +0900

    feat: #13 add YellowRibbon entities

---
+++ src/main/java/laughcandidate/yellowribbonbe/badge/entity/Status.java
@@ -0,0 +1,14 @@
+package laughcandidate.yellowribbonbe.badge.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Status {
+    PENDING("PENDING"),
+    COMPLETE("COMPLETE"),
+    REJECTED("REJECTED");
+
+    private final String status;
+}
