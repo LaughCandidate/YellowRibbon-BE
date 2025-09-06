107e27d78480a09167f0604b15cfa48e92f834a5
hwajin <hvvxzin99@gmail.com>
2025-09-07 03:02:17 +0900

    feat: #13 add YellowRibbon entities

---
+++ src/main/java/laughcandidate/yellowribbonbe/yellowRibbon/entity/YellowRibbon.java
@@ -0,0 +1,26 @@
+package laughcandidate.yellowribbonbe.yellowRibbon.entity;
+
+import jakarta.persistence.*;
+import laughcandidate.yellowribbonbe.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Table(name = "YELLOW_RIBBON")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class YellowRibbon extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "yellow_ribbon_id")
+    private Long id;
+
+    @Column(name = "benefit")
+    private String benefit;
+
+    @Column(name = "season")
+    private int season;
+
+}
