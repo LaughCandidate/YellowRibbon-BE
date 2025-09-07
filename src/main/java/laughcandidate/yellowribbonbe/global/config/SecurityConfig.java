3394a15a88c217a2fba33574c3c4076a885ad8b7
hoonssac <Hoonssac.dev@gmail.com>
2025-09-08 02:02:38 +0900

    feat: #15 add admin API security configuration

--- src/main/java/laughcandidate/yellowribbonbe/global/config/SecurityConfig.java
+++ src/main/java/laughcandidate/yellowribbonbe/global/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/admin/**").hasAuthority(Role.ADMIN.getRole())
 				.requestMatchers("/business/connect/required").hasAuthority(Role.TEMP_USER.getRole())
 				.requestMatchers("/business/connect/optional").hasAuthority(Role.USER.getRole())
 				.requestMatchers(WHITELIST).permitAll()
