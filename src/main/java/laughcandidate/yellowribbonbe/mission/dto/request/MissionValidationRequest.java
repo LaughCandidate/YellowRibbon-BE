4b5ddb74eb0e16e8491f7be240aca4b2570294bc
bronze_yun_4521 <ehddbs452100@naver.com>
2025-09-09 03:24:06 +0900

    feat: #12 validate image with presigned url

--- src/main/java/laughcandidate/yellowribbonbe/mission/dto/request/MissionValidationRequest.java
+++ src/main/java/laughcandidate/yellowribbonbe/mission/dto/request/MissionValidationRequest.java
@@ -1,21 +1,11 @@
 package laughcandidate.yellowribbonbe.mission.dto.request;
 
-import org.springframework.web.multipart.MultipartFile;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import laughcandidate.yellowribbonbe.global.validation.ContentType;
-import laughcandidate.yellowribbonbe.global.validation.FileSize;
 
 public record MissionValidationRequest(
-	@NotNull(message = "이미지 파일은 필수입니다.")
-	@ContentType(
-		allowed = {"image/jpeg", "image/png", "image/gif", "image/webp"},
-		message = "이미지 파일만 업로드 가능합니다. (JPG, PNG, GIF, WEBP)"
-	)
-	@FileSize(
-		max = "5MB",
-		message = "이미지 파일 크기는 5MB 이하여야 합니다."
-	)
-	MultipartFile image
+	@NotNull(message = "이미지 ID는 필수입니다.")
+	@Schema(description = "이미지 ID 식별값", example = "1L")
+	Long imageId
 ) {
 }
