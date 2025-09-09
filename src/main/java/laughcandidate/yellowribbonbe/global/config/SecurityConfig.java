99cb4753a1270f220cd4e5fbc6f39c2c6fe2b0bc
hoonssac <Hoonssac.dev@gmail.com>
2025-09-10 00:47:42 +0900

    feat: #55 add admin requestMatchers for ROLE_ADMIN

--- src/main/java/laughcandidate/yellowribbonbe/global/config/SecurityConfig.java
+++ src/main/java/laughcandidate/yellowribbonbe/global/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import laughcandidate.yellowribbonbe.auth.filter.JwtAuthenticationFilter;
 import laughcandidate.yellowribbonbe.auth.handler.CustomAccessDeniedHandler;
 import laughcandidate.yellowribbonbe.auth.handler.CustomAuthenticationEntryPoint;
 import laughcandidate.yellowribbonbe.auth.jwt.TokenProvider;
+import laughcandidate.yellowribbonbe.user.entity.Role;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
+				.requestMatchers("/admin/**").hasAuthority(Role.ROLE_ADMIN.getRole())
 				.requestMatchers(WHITELIST).permitAll()
 				.requestMatchers(BLACKLIST).authenticated()
 				.anyRequest().authenticated())
