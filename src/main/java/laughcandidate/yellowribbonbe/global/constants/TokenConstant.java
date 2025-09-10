e29791be27d9dfd01d2d6b8cd2759211442a9046
bronze_yun_4521 <ehddbs452100@naver.com>
2025-09-10 21:58:37 +0900

    feat: extend access token

--- src/main/java/laughcandidate/yellowribbonbe/global/constants/TokenConstant.java
+++ src/main/java/laughcandidate/yellowribbonbe/global/constants/TokenConstant.java
@@ -4,7 +4,7 @@ public class TokenConstant {
 
 	public static final int MINUTE_IN_MILLISECONDS = 60 * 1000;
 	public static final long DAYS_IN_MILLISECONDS = 24 * 60 * 60 * 1000L;
-	public static final int ACCESS_TOKEN_EXPIRATION_MINUTE = 20;
+	public static final int ACCESS_TOKEN_EXPIRATION_MINUTE = 20000;
 	public static final int TEMP_ACCESS_TOKEN_EXPIRATION_MINUTE = 30;
 	public static final int REFRESH_TOKEN_EXPIRATION_DAYS = 14;
 	public static final int TEMP_REFRESH_TOKEN_EXPIRATION_DAYS = 1;
